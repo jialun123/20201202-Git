d349acb74c1428e2a2bbb40ac7508c65294abfa9
Karen <13109563292@163.com>
2020-12-02 14:45:23 +0800

    Initial commit

---
+++ src/main/java/org/westos/demo/Test1.java
@@ -0,0 +1,13 @@
+package org.westos.demo;
+
+/**
+ * @Author: JiaLun
+ * @Description:
+ * @Date: Created in 2020/12/2 14:42
+ * @Modified By:
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("主分支");
+    }
+}
