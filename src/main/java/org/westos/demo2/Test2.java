ef5c16df8873d6df57c5a4adef0d0eddd08e04a1
Karen <13109563292@163.com>
2020-12-02 14:48:29 +0800

    dev

---
+++ src/main/java/org/westos/demo2/Test2.java
@@ -0,0 +1,13 @@
+package org.westos.demo2;
+
+/**
+ * @Author: JiaLun
+ * @Description:
+ * @Date: Created in 2020/12/2 14:47
+ * @Modified By:
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("dev分支");
+    }
+}
